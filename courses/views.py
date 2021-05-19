d1acbbf9df838be6ee70f988bfb45d320ed7e8e6
Fredrick Ojure <fredojure@hotmail.com>
2021-05-19 15:56:37 +0300

    Added urls to courses

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.urls.import reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
