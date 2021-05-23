006090e304400d32e578bcd28ec521a090d3784f
Fredrick Ojure <fredojure@hotmail.com>
2021-05-23 20:39:42 +0300

    Added nested Module view to courses api

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,10 +1,11 @@
 from rest_framework import generics
 from ..models import Subject
 from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
-    serilizer_class = SubjectSerializer
+    serializer_class = SubjectSerializer
 
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
