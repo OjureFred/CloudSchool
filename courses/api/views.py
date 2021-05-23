3e9ebb20be683dfa61668318bef5e1191a70cc0d
Fredrick Ojure <fredojure@hotmail.com>
2021-05-23 20:52:27 +0300

    Added authentication to api classes

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework import generics
-from ..models import Subject
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .serializers import SubjectSerializer
 from django.shortcuts import get_object_or_404
+
+from ..models import Subject, Course
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -10,3 +15,11 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
