d85a0f75b0c89d5b66bb24da4c962f03b09f52d8
Fredrick Ojure <fredojure@hotmail.com>
2021-05-24 10:38:21 +0300

    Created view to consume course contents api

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,3 +1,4 @@
+from courses.api.permissions import IsEnrolled
 from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,7 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
 
-from .serializers import CourseSerializer, SubjectSerializer
+from .permissions import IsEnrolled
+from .serializers import CourseSerializer, SubjectSerializer, CourseWithContentsSerializer
 from ..models import Subject, Course
 
 class SubjectListView(generics.ListAPIView):
@@ -26,3 +28,8 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+    
+    @action(detail=True, methods=['get'], serializer_class=CourseWithContentsSerializer, 
+    authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
